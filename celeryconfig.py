bf9b504d92f8029ff9803d458a7bac7e40a2eedf
Jerry <lxb429@gmail.com>
2011-04-26 02:01:54 +0800

    配置nginx

--- celeryconfig.py
+++ celeryconfig.py
@@ -3,20 +3,20 @@
 
 BROKER_BACKEND = "mongodb"
 BROKER_HOST = "localhost"
-BROKER_PORT = 7680
+BROKER_PORT = 27017
 
 CELERY_RESULT_BACKEND = "mongodb"
 CELERY_MONGODB_BACKEND_SETTINGS = {
     "host": "localhost",
-    "port": 7680,
+    "port": 27017,
     "database": "books",
     "taskmeta_collection": "celerytasks",
 }
 
-CELERYD_CONCURRENCY = 10
+CELERYD_CONCURRENCY = 5
 
-CELERYD_LOG_LEVEL = "INFO"
-CELERYD_LOG_FILE = "logs/celery.log"
+# CELERYD_LOG_LEVEL = "INFO"
+# CELERYD_LOG_FILE = "logs/celery.log"
 
 
 CELERY_IMPORTS = ("celerytasks", )
